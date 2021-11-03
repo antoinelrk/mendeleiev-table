9bbf394d8c9ffb9cd9b3208ac385bdfeab6094a2
Antoine 'Kwd' LRK <freelance@panthera.studio>
2021-11-03 14:57:48 +0100

    Move project to NuxtJS2 App

--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.setPublicPath('public/');
-mix.disableNotifications();
-
-mix.js('resources/js/app.js', 'public/js/');
-mix.sass('resources/scss/app.scss', 'public/css');
